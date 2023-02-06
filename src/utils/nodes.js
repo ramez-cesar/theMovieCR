4d24d9c939ddd96519b57fc06a8af8028665e4ea
Cesar <cesarramez8@gmail.com>
2023-02-06 13:29:57 -0600

    Add nodes.js file in utils folder

---
+++ src/utils/nodes.js
@@ -0,0 +1,41 @@
+const nameSite = document.querySelector("section.header--name-site > h1")
+
+const mainViewContainer = document.querySelector('.main-view-container')
+
+const mainSearchContainer = document.querySelector('.main--search-container')
+const searchContainer = document.querySelector('.search--container')
+const searchMovieMian = document.querySelector('.search-movie-main')
+const btnSearchMovieMain = document.querySelector('#btn_search_movie_main')
+
+
+const mainTrendsContainer = document.querySelector('.main--trends-container')
+const btnShowTrends = document.querySelector('#btn_show_trends')
+const trendsContainerMovies = document.querySelector('.trends-container--movies')
+
+
+const mainCategoryContainer = document.querySelector('.main--category-container')
+const categoryContainer = document.querySelector('.category-container')
+
+
+const SPAContainerSearchMovie = document.querySelector('.SPA-container--search-movie')
+const searchMovieSPA = document.querySelector('.search-movie-spa')
+const btnSearchMovieSPA = document.querySelector('#btn_search_movie_spa')
+const SPASearchMovie = document.querySelector('.SPA--search-movie')
+
+const SPAContainerMovieList = document.querySelector('.SPA-container--movie-list')
+const SPAMovieList = document.querySelector('.SPA--movie-list')
+
+
+const SPAContainerMovieDetails = document.querySelector('.SPA-container--movie-details')
+const moviePoster = document.querySelector('.movie-poster')
+const movieTitle = document.querySelector('.movie-title')
+const movieRating = document.querySelector('.movie-rating')
+const movieDate = document.querySelector('.movie-date')
+const movieCategory = document.querySelector('.movie-category')
+const movieDescription = document.querySelector('.movie-description')
+const SPASimilarMovies = document.querySelector('.SPA--similar-movies')
+
+
+const SPAContainerMoviesByCategory = document.querySelector('.SPA-container--movies-by-category')
+const SPAMoviesByCategory = document.querySelector('.SPA--movies-by-category')
+const SPACategoryName = document.querySelector('.SPA--category-name')
