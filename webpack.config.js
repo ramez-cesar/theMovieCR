694de14f2389be0c865c7ed204beb3c8e632cd5a
Cesar <cesarramez8@gmail.com>
2023-02-10 15:09:09 -0600

    Add webpack.config.js in the project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        clean: true
+    }, 
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    },
+    plugins: [
+        
+    ]
+}
