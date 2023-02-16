3543f1c16333c4c9a7d86b9353c500a81410ae79
Cesar <cesarramez8@gmail.com>
2023-02-16 14:19:32 -0600

    Add import for css styles in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Template from "./template/main.js";
-// import '../src/styles/style.css'
+import '../src/styles/style.css'
 
 (async function App() {
     const view = document.querySelector('#mian')
